baa8f94409c9281faa98a550a0184981171b3538
praveen <praveencpp@gmail.com>
2015-10-18 21:25:09 +0530

    kernel module parameter is added

--- ldd/hello_module/hello-module.c
+++ ldd/hello_module/hello-module.c
@@ -3,12 +3,18 @@
 #include <linux/kernel.h>
 #include <linux/moduleparam.h>
 
-
+static int hello_arg=89 ;
+module_param(hello_arg,int,0644);
+/*
+# insmod hello_module.ko hello_arg=100
+89 value wiil be ignored if we provide module param
+*/
 
 static  int __init hello_entry(void)
 {
 
 	printk(KERN_INFO "hello entry\n");
+	printk(KERN_INFO "hello entry: module parameter: %d\n",hello_arg);
 	return 0;
 }
 
